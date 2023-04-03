e4c06982f01d37558f72ad69b868a67e718f1991
PedrodeParquez <egor.mashin.04@mail.ru>
2023-04-04 03:49:27 +0700

    add: class, subclases and overridden methods

---
+++ PDF.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SecondLab {
+  class PDF : Document {
+    string DocumentPDFVersion;
+    int DocumentPDFNumberOfPages;
+
+    public PDF(string DocumentName, string DocumentAuthor, string DocumentKeyWords,
+      string DocumentTopic, string DocumentPath) {
+      this.DocumentName = DocumentName;
+      this.DocumentAuthor = DocumentAuthor;
+      this.DocumentKeyWords = DocumentKeyWords;
+      this.DocumentTopic = DocumentTopic;
+      this.DocumentPath = DocumentPath;
+
+      Console.Write("Введите версию PDF файл: ");
+      DocumentPDFVersion = Console.ReadLine();
+
+      Console.WriteLine("Введите количество страниц: ");
+      DocumentPDFNumberOfPages = Convert.ToInt32(Console.ReadLine());
+    }
+     
+    public override void DocumentInfo() {
+      Console.WriteLine($"Имя файла: {DocumentName}\nАвтор: {DocumentAuthor}\nТема: {DocumentTopic} + " + 
+        $"Ключевые слова: {DocumentKeyWords}\nПуть к файлу: {DocumentPath}\nВерсия PDF: {DocumentPDFVersion}" +
+        $"\nКоличество страниц: {DocumentPDFNumberOfPages}");
+    }
+  }
+}
---
+++ TXT.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SecondLab {
+  class TXT : Document {
+    int DocumentTxtFileNumberOfRows, DocumentTxtFileNumberOfSymbols;
+    
+    public TXT(string DocumentName, string DocumentAuthor, string DocumentKeyWords,
+      string DocumentTopic, string DocumentPath) {
+      this.DocumentName = DocumentName
+      this.DocumentAuthor = DocumentAuthor;
+      this.DocumentKeyWords = DocumentKeyWords;
+      this.DocumentTopic = DocumentTopic;
+      this.DocumentPath = DocumentPath;
+
+      Console.WriteLine("Введите количество символов: ");
+      DocumentTxtFileNumberOfSymbols = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Введите количество строк: ");
+      DocumentTxtFileNumberOfRows = Convert.ToInt32(Console.ReadLine());    
+    }
+
+    public override void DocumentInfo() {
+      Console.WriteLine($"Имя файла: {DocumentName}\nАвтор: {DocumentAuthor}\nТема: {DocumentTopic} + " +
+        $"Ключевые слова: {DocumentKeyWords}\nПуть к файлу: {DocumentPath}\nКоличество символов: {DocumentTxtFileNumberOfSymbols}" +
+        $"\nКоличество строк: {DocumentTxtFileNumberOfRows}");
+    }
+  }
+}
