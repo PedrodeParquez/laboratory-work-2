8efd712ca00785fc376d7175eb21c670784a3062
PedrodeParquez <egor.mashin.04@mail.ru>
2023-06-05 17:21:02 +0700

    add: singletone main

--- EXCEL.cs
+++ EXCEL.cs
@@ -4,23 +4,23 @@ namespace SecondLab {
   class EXCEL : Document {
     int DocumentExcelFileNumberOfRows, DocumentExcelFileNumberOfColumns;
 
-    public EXCEL(string DocumentName, string DocumentAuthor, string DocumentKeyWords, 
-      string DocumentTopic, string DocumentPath) {
-      this.DocumentName = DocumentName
+    public EXCEL(string DocumentName, string DocumentAuthor, string DocumentKeyWords,
+      string DocumentPath, string DocumentTopic) {
+      this.DocumentName = DocumentName;
       this.DocumentAuthor = DocumentAuthor;
       this.DocumentKeyWords = DocumentKeyWords;
-      this.DocumentTopic = DocumentTopic;
       this.DocumentPath = DocumentPath;
+      this.DocumentTopic = DocumentTopic;
 
       Console.Write("Введите количество строк: ");
-      DocumentExcelFileNumberOfRows = Convert.ToInt32(Console.ReadLine());
+      DocumentExcelFileNumberOfRows = int.Parse(Console.ReadLine());
 
       Console.WriteLine("Введите количество столбцов: ");
-      DocumentExcelFileNumberOfColumns = Convert.ToInt32(Console.ReadLine());
+      DocumentExcelFileNumberOfColumns = int.Parse(Console.ReadLine());
     }
 
     public override void DocumentInfo() {
-      Console.WriteLine($"Имя файла: {DocumentFile}\nАвтор: {DocumentAuthor}\nТема: {DocumentTopic} + " +
+      Console.WriteLine($"Имя файла: {DocumentName}\nАвтор: {DocumentAuthor}\nТема: {DocumentTopic} + " +
         $"Ключевые слова: {DocumentKeyWords}\nПуть к файлу: {DocumentPath}\nКоличество строк: {DocumentExcelFileNumberOfRows}" +
         $"\nКоличество столбцов: {DocumentExcelFileNumberOfColumns}");
     }
